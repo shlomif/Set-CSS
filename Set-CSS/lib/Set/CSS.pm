d0f1d5bdc6d786c2c192a5284b6c96cc469e9e2f
Shlomi Fish <shlomif@shlomifish.org>
2021-12-13 20:20:44 +0200

    add skel

--- Set-CSS/lib/Set/CSS.pm
+++ Set-CSS/lib/Set/CSS.pm
@@ -3,4 +3,27 @@ package Set::CSS;
 use strict;
 use warnings;
 
+use parent 'Set::Object';
+
 1;
+
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+Set::CSS - set of CSS classes
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 METHODS
+
+=head2
+
+=head2
+
+=cut
+
